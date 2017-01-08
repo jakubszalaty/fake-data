62a4be6f35eac3771e0f69e1b869e2d1e87e7220
Kuba Szalaty <nnaisur@gmail.com>
2017-01-08 22:55:24 +0100

    Added new routing

--- server.js
+++ server.js
@@ -30,6 +30,31 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/users.json', function (req, res) {
+  let users = []
+  let length = req.query.length
+
+  if(!length) length = 10
+  else if(length > 50) length = 50
+
+  for (var i = 0; i < length; i++) {
+    const firstName = faker.name.firstName()
+    const lastName = faker.name.lastName()
+    users.push({
+      id: i+1,
+      firstName: firstName,
+      lastName: lastName,
+      email: faker.internet.email(firstName,lastName),
+      phone: faker.phone.phoneNumber(),
+      avatar: faker.image.avatar()
+
+    })
+
+  }
+
+
+  res.json(users)
+})
 app.get('/numbers.json', function (req, res) {
   let numbers = { data:[] }
   let length = req.query.length
